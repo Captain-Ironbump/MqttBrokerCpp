c66ae70102ed4222a56eae76d70cf8f1e6d6a88e
Lukas Linde <linde.lukas.99@gmail.com>
2024-11-26 00:54:05 +0100

    included updated logger library

--- include/logger.hpp
+++ include/logger.hpp
@@ -3,7 +3,6 @@
 
 // #include <ctime>
 #include <fstream>
-#include <sstream>
 
 enum LogLevel {
     DEBUG,
@@ -16,14 +15,17 @@ enum LogLevel {
 class Logger {
 private:
     // Variables
+    size_t maxFileSize = 1024 * 1024 * 10; // 10 MB
     std::ofstream* logFile = nullptr;
+    int fileCount;
     // Functions
     std::string levelToString(LogLevel level);
     std::string getColorForLevel(LogLevel level);
 public:
-    Logger(const std::string& filename);
+    Logger(const std::string& baseFileName);
     ~Logger();
     void log(LogLevel level, const std::string& message, int useLogFile=0);
+    std::string getNextLogFile(const std::string& baseFileName);
 };
 
 #endif
